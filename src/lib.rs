f7eccb74a3003d185cc6ba9e4b34b1156b495cc3
Maarten de Vries <maarten@de-vri.es>
2020-07-02 20:19:28 +0200

    Add module level disable_sigpipe() function.

--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,22 @@ pub use address::*;
 
 mod socket;
 pub use socket::*;
+
+/// Disable SIGPIPE for the current process.
+///
+/// Writing to a closed socket may cause a SIGPIPE to be sent to the process (depending on the socket type).
+/// On most platforms this is prevented, either by using the `MSG_NOSIGNAL` flag when writing
+/// or by setting the `SO_NOSIGPIPE` socket option.
+///
+/// However, if a platform does not support `MSG_NOSIGNAL` or `SO_NOGSISPIPE`,
+/// the signal needs to be handled or the process will be terminated by the kernel.
+/// Calling [`disable_sigpipe()`] make sure the signal is ignored without terminating the process.
+pub fn disable_sigpipe() -> std::io::Result<()> {
+	unsafe {
+		if libc::signal(libc::SIGPIPE, libc::SIG_IGN) == libc::SIG_ERR {
+			Err(std::io::Error::last_os_error())
+		} else {
+			Ok(())
+		}
+	}
+}
